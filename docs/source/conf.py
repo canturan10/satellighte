cfd418c668ea35942513b5a8b3de1c0ee01a0906
Kenobi <can.turan.10@gmail.com>
2022-02-20 00:41:41 +0300

    unused ext remove

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,6 @@ extensions = [
     "sphinx.ext.viewcode",  # Add a link to the Python source code for classes, functions etc.
     "sphinx_autodoc_typehints",  # Automatically document param types (less noise in class signature)
     "nbsphinx",  # Integrate Jupyter Notebooks and Sphinx
-    "IPython.sphinxext.ipython_console_highlighting",
     "sphinxemoji.sphinxemoji",
     "sphinxcontrib.napoleon",
     "recommonmark",
